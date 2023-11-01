d3f1e8f1561b5d2af1e5268d4b9b44594b8dd9b6
leonsal <leonel.salvadori@gmail.com>
2023-11-01 13:49:17 -0300

    ...

--- run_tests.c
+++ run_tests.c
@@ -5,20 +5,20 @@
 #include "cx_tests.h"
 
 #define cx_str_cap 16
-#define cx_str_name s16
+#define cx_str_name str16
 //#define cx_str_allocator
 #define cx_str_implement
 #include "cx_str.h"
 
 #define cx_str_cap 32
-#define cx_str_name s32
+#define cx_str_name str32
 //#define cx_str_allocator
 #define cx_str_implement
 #include "cx_str.h"
 
 int main() {
 
-    s16 s1 = s16_init();
+    str16 s1 = str16_init();
     //CxStr s1 = CxStr_init2(cxDefaultAllocator());
     // cxAllocBlockTests();
     // cxArrayTests();
