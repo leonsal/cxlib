e61c2c03bb1589c38263ced9f7670bad89549757
leonsal <leonel.salvadori@gmail.com>
2023-11-08 13:33:37 -0300

    ...

--- tests/main.c
+++ tests/main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
-#include "alloc.h"
-#include "array.h"
-#include "hmap.h"
-#include "string.h"
+// #include "alloc.h"
+// #include "array.h"
+// #include "hmap.h"
+// #include "string.h"
 
-#define cx_str_name str
+#define cx_str_name cxstr
+#define cx_str_cap 8
 #define cx_str_implement
 #include "cx_str.h"
 
 int main() {
 
-    str a1 = str_init();
-    str_cpy(&a1, "test");
+    cxstr a1 = cxstr_init();
+    cxstr_cpy(&a1, "test");
+    cxstr_free(&a1);
 
-    cxAllocBlockTests();
-    cxArrayTests();
-    cxHmapTests();
-    cxStrTests();
+    // cxAllocBlockTests();
+    // cxArrayTests();
+    // cxHmapTests();
+    // cxStrTests();
     return 0;
 }
 
