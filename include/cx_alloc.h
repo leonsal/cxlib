5aacfd33643b88a98ceab1ff07dee48b6749c1b0
leonsal <leonel.salvadori@gmail.com>
2024-01-07 17:09:19 -0300

    ...

--- include/cx_alloc.h
+++ include/cx_alloc.h
@@ -10,7 +10,7 @@ typedef void  (*CxAllocatorFreeFn)(void*, void*, size_t);
 typedef struct CxAllocator {
     void* ctx;                  // Allocator internal state
     CxAllocatorAllocFn alloc;   // Allocates and returns pointer
-    CxAllocatorFreeFn free;     // Free previous allocated pointer
+    CxAllocatorFreeFn free;     // Free previous allocated pointer (may be a NOOP)
 } CxAllocator;
 
 //
@@ -18,6 +18,7 @@ typedef struct CxAllocator {
 //
 const CxAllocator* cxDefaultAllocator();
 
+// Macros for allocating and freeing from an Allocator
 #define cx_alloc_malloc(a,n)    a->alloc(a->ctx,n)
 #define cx_alloc_free(a,p,n)    a->free(a->ctx,p,n)
 
