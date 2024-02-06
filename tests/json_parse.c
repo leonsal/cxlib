1eecb0397cd9db7f668fef8c86515be62a1992b0
leonsal <leonel.salvadori@gmail.com>
2024-02-06 18:41:55 -0300

    ...

--- tests/json_parse.c
+++ tests/json_parse.c
@@ -15,10 +15,10 @@ void json_parse_tests(void) {
     // Use default 'malloc/free' allocator
     json_parse_test(cxDefaultAllocator());
 
-    // // Use pool allocator
-    // CxPoolAllocator* pa = cx_pool_allocator_create(4*1024, NULL);
-    // json_build_test(cx_pool_allocator_iface(pa));
-    // cx_pool_allocator_destroy(pa);
+    // Use pool allocator
+    CxPoolAllocator* pa = cx_pool_allocator_create(4*1024, NULL);
+    json_parse_test(cx_pool_allocator_iface(pa));
+    cx_pool_allocator_destroy(pa);
 }
 
 void json_parse_test(const CxAllocator* alloc) {
