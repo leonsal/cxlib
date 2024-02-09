34a4d8a49459bccf6cbaee3e74f0ff14399dffa3
leonsal <leonel.salvadori@gmail.com>
2024-02-08 21:50:02 -0300

    ...

--- tests/var.c
+++ tests/var.c
@@ -15,10 +15,10 @@ void cx_var_tests(void) {
     // Use default 'malloc/free' allocator
     cx_var_test(cxDefaultAllocator());
 
-    // // Use pool allocator
-    // CxPoolAllocator* pa = cx_pool_allocator_create(4*1024, NULL);
-    // cx_var_test(cx_pool_allocator_iface(pa));
-    // cx_pool_allocator_destroy(pa);
+    // Use pool allocator
+    CxPoolAllocator* pa = cx_pool_allocator_create(4*1024, NULL);
+    cx_var_test(cx_pool_allocator_iface(pa));
+    cx_pool_allocator_destroy(pa);
 }
 
 void cx_var_test(const CxAllocator* alloc) {
