6f24ff032b1c49acfb1de648e62620e30c540f2d
leonsal <leonel.salvadori@gmail.com>
2024-02-09 15:40:50 -0300

    ...

--- include/cx_json_parse.h
+++ include/cx_json_parse.h
@@ -4,10 +4,6 @@
 #include "cx_alloc.h"
 #include "cx_var.h"
 
-// Type for JSON parse configuration
-typedef struct CxJsonBuildCfg {
-} CxJsonParseCfg;
-
 // Parses JSON string and builds CxVar.
 // Return non-zero error code
 int cx_json_parse(const char* data, size_t len, CxVar** var, const CxAllocator* alloc); 
