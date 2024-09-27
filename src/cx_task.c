bf5afd9473c5e42122b33c76b604cef4a8db6d02
leonsal <leonel.salvadori@gmail.com>
2024-09-27 10:01:55 -0300

    ...

--- src/cx_task.c
+++ src/cx_task.c
@@ -5,24 +5,26 @@
 #include "cx_task.h"
 
 typedef struct CxTaskRunner {
-    size_t          nthreads;
-    CxThreadPool*   tpool;
-    bool            started;
-    size_t          cycles;
-    size_t          run_cycles;
+    const CxAllocator*  alloc;
+    size_t              nthreads;
+    CxThreadPool*       tpool;
+    bool                started;
+    size_t              cycles;
+    size_t              run_cycles;
 } CxTaskRunner;
 
 CxTaskRunner* cx_task_runner_new(const CxAllocator* alloc, size_t nthreads) {
 
     CXCHK(nthreads > 1);
     CxTaskRunner* tr = cx_alloc_mallocz(alloc, sizeof(CxTaskRunner));
+    tr->alloc = alloc;
     tr->nthreads = nthreads;
     return tr;
 }
 
 CxError cx_task_runner_del(CxTaskRunner* tr) {
 
-
+    cx_alloc_free(tr->alloc, tr, sizeof(CxTaskRunner));
     return CXOK();
 }
 
