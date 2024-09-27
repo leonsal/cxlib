b1a3a5d0b5ad6ed490111e4fd1b940f16286886a
leonsal <leonel.salvadori@gmail.com>
2024-09-27 18:44:07 -0300

    ...

--- include/cx_task.h
+++ include/cx_task.h
@@ -7,23 +7,22 @@
 #include "cx_error.h"
 #include "cx_alloc.h"
 
-// Creates task runner using specified custom allocator and maximum number of threads
+// Creates task flow using specified custom allocator and maximum number of threads
 typedef struct CxTaskFlow CxTaskFlow;
 CxTaskFlow* cx_task_flow_new(const CxAllocator* alloc, size_t nthreads);
 
-// Finishes all threads and destroys the previously created task runneruler
-// Returns non-zero system error code
-CxError cx_task_flow_del(CxTaskFlow* ts);
+// Finishes all tasks in the current cycle and then destroys the task flow
+CxError cx_task_flow_del(CxTaskFlow* tf);
 
 // Run tasks for the specified number of cycles.
-// If cycles is 0, runs till cx_task_runner_stop() is calledd
-CxError cx_task_flow_start(CxTaskFlow* ts, size_t cycles);
+// If cycles is 0, runs till cx_task_runner_stop() is called
+CxError cx_task_flow_start(CxTaskFlow* tf, size_t cycles);
 
 // Waits for the current cycle to run and then stops the task runner.
-CxError cx_task_flow_stop(CxTaskFlow* ts);
+CxError cx_task_flow_stop(CxTaskFlow* tf);
 
 // Returns the number of cycles run
-size_t cx_task_flow_cycles(CxTaskFlow* ts);
+size_t cx_task_flow_cycles(CxTaskFlow* tf);
 
 // Creates task which will be run in the specified task runner.
 // The task runner must be stopped.
@@ -31,7 +30,7 @@ typedef void (*CxTaskFlowTask)(void*);
 CxError cx_task_flow_add_task(CxTaskFlow* tf, const char* name, CxTaskFlowTask fn, void* arg, size_t* tid);
 
 // Sets the dependency of a task
-CxError cx_task_flow_task_dep(CxTaskFlow* tr, size_t tid, size_t other);
+CxError cx_task_flow_task_dep(CxTaskFlow* tf, size_t tid, size_t other);
 
 
 #endif
